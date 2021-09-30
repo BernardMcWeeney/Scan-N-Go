4217d38d2b7e664e839d8d70dbe90d59cce2209c
shaun-kee-rkee <77441804+shaun-kee-rkee@users.noreply.github.com>
2021-09-30 13:16:18 +0100

    Django Setup Complete

---
+++ backend/shop/models.py
@@ -0,0 +1,142 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class APIUser(AbstractUser):
+    pass
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, null=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)
+    description = models.CharField(max_length=5000, null=True)
+    productImage = models.FileField(upload_to='images/')
+    '''
+    Product tags to be decided. 
+    productTag = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    ]
+    '''
+
+class Basket(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(APIUser, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    dateCreated = models.DateTimeField(auto_now_add=True)
+
+class BasketItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    basket_id = models.ForeignKey(Basket, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    dateCreated = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def getCalculatedTotal(self):
+        calculatedTotal = self.quantity * self.product_id.price
+        return calculatedTotal
+
+    def save(self, *args, **kwargs):
+        self.calculatedTotal = self.getCalculatedTotal
+        super(BasketItem, self).save(*args, **kwargs)
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    basket_id = models.ForeignKey(Basket, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(APIUser, on_delete=models.CASCADE)
+    total_price = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)
+    status = [
+        ('Prepared', 'Prepared'),
+        ('Order', 'Order'),
+        ('Processing', 'Processing'),
+        ('Shipped', 'Shipped'),
+        ('Complete', 'Complete'),
+        ('Refunded', 'Refunded'),
+        ('Issue', 'Issue'),
+    ]
+    payment_status = [
+        ('NotReceived', 'Not Received'),
+        ('Received', 'Received'),
+    ]
+    customer_order_notes = models.CharField(max_length=5000, null=True)
+    internal_order_notes = models.CharField(max_length=5000, null=True)
+
+class IrishBillingAddress(models.Model):
+    user_id = models.ForeignKey(APIUser, on_delete=models.CASCADE, null=False)
+    contact_name = models.CharField("Contact name", max_length=102, null=False)
+    company_name = models.CharField("Company name", max_length=102, null=True)
+    address_line1 = models.CharField("Address line 1", max_length=1024, null=False)
+    address_line2 = models.CharField("Address line 2", max_length=1024, blank=True, null=False)
+    address_line3 = models.CharField("Address line 3", max_length=1024, blank=True, null=True)
+    eir_code = models.CharField("Eir Code", max_length=8, null=False)
+    county = [
+        ('Carlow', 'Carlow'),
+        ('Cavan', 'Cavan'),
+        ('Clare', 'Clare'),
+        ('Cork', 'Cork'),
+        ('Donegal', 'Donegal'),
+        ('Dublin', 'Dublin'),
+        ('Galway', 'Galway'),
+        ('Kerry', 'Kerry'),
+        ('Kildare', 'Kildare'),
+        ('Kilkenny', 'Kilkenny'),
+        ('Laois', 'Laois'),
+        ('Leitrim', 'Leitrim'),
+        ('Limerick', 'Limerick'),
+        ('Longford', 'Longford'),
+        ('Louth', 'Louth'),
+        ('Mayo', 'Mayo'),
+        ('Meath', 'Meath'),
+        ('Monaghan', 'Monaghan'),
+        ('Offaly', 'Offaly'),
+        ('Roscommon', 'Roscommon'),
+        ('Sligo', 'Sligo'),
+        ('Tipperary', 'Tipperary'),
+        ('Waterford', 'Waterford'),
+        ('Westmeath', 'Westmeath'),
+        ('Wexford', 'Wexford'),
+        ('Wicklow', 'Wicklow'),
+    ]
+
+class IrishShippingAddress(models.Model):
+    user_id = models.ForeignKey(APIUser, on_delete=models.CASCADE, null=False)
+    contact_name = models.CharField("Contact name", max_length=102, null=False)
+    company_name = models.CharField("Company name", max_length=102, null=True)
+    address_line1 = models.CharField("Address line 1", max_length=1024, null=False)
+    address_line2 = models.CharField("Address line 2", max_length=1024, blank=True, null=False)
+    address_line3 = models.CharField("Address line 3", max_length=1024, blank=True, null=True)
+    eir_code = models.CharField("Eir Code", max_length=8, null=False)
+    county = [
+        ('Carlow', 'Carlow'),
+        ('Cavan', 'Cavan'),
+        ('Clare', 'Clare'),
+        ('Cork', 'Cork'),
+        ('Donegal', 'Donegal'),
+        ('Dublin', 'Dublin'),
+        ('Galway', 'Galway'),
+        ('Kerry', 'Kerry'),
+        ('Kildare', 'Kildare'),
+        ('Kilkenny', 'Kilkenny'),
+        ('Laois', 'Laois'),
+        ('Leitrim', 'Leitrim'),
+        ('Limerick', 'Limerick'),
+        ('Longford', 'Longford'),
+        ('Louth', 'Louth'),
+        ('Mayo', 'Mayo'),
+        ('Meath', 'Meath'),
+        ('Monaghan', 'Monaghan'),
+        ('Offaly', 'Offaly'),
+        ('Roscommon', 'Roscommon'),
+        ('Sligo', 'Sligo'),
+        ('Tipperary', 'Tipperary'),
+        ('Waterford', 'Waterford'),
+        ('Westmeath', 'Westmeath'),
+        ('Wexford', 'Wexford'),
+        ('Wicklow', 'Wicklow'),
+    ]
