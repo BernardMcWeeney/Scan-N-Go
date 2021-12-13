930bd3bc85b0f2b470ce865d8cea0de1fa4299df
Bernard McWeeney <bernardmcweeney@Bernards-Mini.station>
2021-12-13 17:54:44 +0000

    saving progress - trobuleshooting erros with digital reciept

--- frontend/public/javascripts/getorderinfo.js
+++ frontend/public/javascripts/getorderinfo.js
@@ -16,33 +16,3 @@ function backendServer() {
 }
 
 
-function GetUserOrder() {
-    let backendServerURL = backendServer()
-    let djangoServer = backendServerURL + "orders/"
-    let token = sessionStorage.getItem('access').toString()
-
-    var obj = {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Origin': '',
-        'Host': 'api.producthunt.com',
-        'Authorization': 'Bearer ' + token
-      },}
-
-    console.log("sending data to ",djangoServer)
-    fetch(djangoServer, obj)
-      .then(response => response.json()) // extract the json from the response you get from the server
-      .then(data => {
-
-          let UserOrderData = data;
-          console.log(UserOrderData)
-          for (var i = 0; i < UserOrderData.length; i++) {
-              console.log(UserOrderData[i])
-          }
-      })
-
-}
-
-    GetUserOrder()
