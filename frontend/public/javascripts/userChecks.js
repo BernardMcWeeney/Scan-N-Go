ebb42ab102d2b9e6b1966cfb9877af955a63903e
kees2 <shaun.kee2@mail.dcu.ie>
2022-04-05 16:32:09 +0100

    pages now checks logins / stores to validate users

--- frontend/public/javascripts/userChecks.js
+++ frontend/public/javascripts/userChecks.js
@@ -9,3 +9,17 @@ function redirectUser() {
         location.href = '/';
     }
 }
+
+function redirectStoreLoggedInUser() {
+    if (sessionStorage.getItem('storeID') !== '5' && sessionStorage.getItem('storeID') !== null) {
+        location.href = '/';
+    }
+}
+
+function redirectNonStoreLoggedInUser() {
+    if (sessionStorage.getItem('storeID') === '5') {
+        location.href = '/store-scanner';
+    } else if (sessionStorage.getItem('storeID') === null) {
+        location.href = '/store-scanner';
+    }
+}
